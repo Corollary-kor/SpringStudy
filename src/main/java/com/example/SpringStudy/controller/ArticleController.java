a0241f9ad53993e377fa781c4634aea8a9372b6f
dongseok <joker0304@gmail.com>
2023-11-15 11:43:14 +0900

    11.14 study

--- src/main/java/com/example/SpringStudy/controller/ArticleController.java
+++ src/main/java/com/example/SpringStudy/controller/ArticleController.java
@@ -3,12 +3,14 @@ package com.example.SpringStudy.controller;
 import com.example.SpringStudy.dto.ArticleForm;
 import com.example.SpringStudy.entity.Article;
 import com.example.SpringStudy.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
@@ -19,13 +21,17 @@ public class ArticleController {
     }
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
-        System.out.println(form.toString());
+//        System.out.println(form.toString());
+        log.info(form.toString());
+
         // 1. DTO를 엔티티로 변환
         Article article = form.toEntity();
-        System.out.println(article.toString());
+//        System.out.println(article.toString());
+        log.info(article.toString());
         // 2. 리파지터리로 엔티티를 DB로 저장
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+//        System.out.println(saved.toString());
+        log.info(saved.toString());
         return "";
     }
 }
