9808c182a3897f421c1ecde2ee212119693803b9
alice <ldshoo@naver.com>
2023-11-18 00:47:35 +0900

    11.17

--- src/main/java/com/example/SpringStudy/repository/ArticleRepository.java
+++ src/main/java/com/example/SpringStudy/repository/ArticleRepository.java
@@ -3,6 +3,10 @@ package com.example.SpringStudy.repository;
 import com.example.SpringStudy.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article, Long> {
+    @Override
+    ArrayList<Article> findAll();
 }
 
