6c05d6bfeed73490524cb5bbf8ac38b18ad7ab32
alice <ldshoo@naver.com>
2023-12-03 22:44:12 +0900

    12.03

--- src/main/java/com/example/SpringStudy/entity/Comment.java
+++ src/main/java/com/example/SpringStudy/entity/Comment.java
@@ -1,5 +1,6 @@
 package com.example.SpringStudy.entity;
 
+import com.example.SpringStudy.dto.CommentDto;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -25,5 +26,26 @@ public class Comment {
 
     @Column
     private String body; // 댓글 본문
+
+    public static Comment createComment(CommentDto dto, Article article) {
+        //예외 발생
+        if(dto.getId() != null)
+            throw new IllegalArgumentException("댓글 생성 실패! 댓글 id가 없어야 합니다.");
+        if(dto.getArticleId() != article.getId())
+            throw new IllegalArgumentException("댓글 생성 실패! 댓글 id가 잘못됐습니다.");
+        //엔티티 생성 및 반환
+        return new Comment(dto.getId(),article, dto.getNickname(), dto.getBody());
+    }
+
+    public void patch(CommentDto dto) {
+        //예외 발생
+        if(this.getId() != dto.getId())
+            throw new IllegalArgumentException("댓글 수정 실패! 잘못된 id가 입력됐습니다.");
+        //객체 생성
+        if(dto.getNickname() != null)
+            this.nickname = dto.getNickname();
+        if(dto.getBody() != null)
+            this.body = dto.getBody();
+    }
 
 }
